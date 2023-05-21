a19156b0eeb6412b90c580aa9385dd3ec10ffbb8
Willian Araujo <wil.araujo20@gmail.com>
2023-05-21 21:00:36 +0100

    About view

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module stocktracker {
 	requires javafx.controls;
 	requires java.sql;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.graphics, javafx.fxml;
 }
