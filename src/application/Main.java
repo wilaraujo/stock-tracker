b0d265c06798838dec85d078483f9b3efecfce90
Willian Araujo <wil.araujo20@gmail.com>
2023-05-21 15:53:52 +0100

    Main view controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 	
+import java.sql.Connection;
+
+import db.DB;
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -21,6 +24,9 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+		
 		launch(args);
 	}
 }
